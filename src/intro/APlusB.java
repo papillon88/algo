6bc75d07f211ca288290c1d4cb63e9167aac1ff4
papillon <devopreet.ch@gmail.com>
2017-02-17 14:51:38 -0600

    week 2

---
+++ src/intro/APlusB.java
@@ -0,0 +1,16 @@
+package intro;
+
+import java.util.Scanner;
+
+public class APlusB {
+  public static void main(String[] args) {
+    try(Scanner s = new Scanner(System.in)){
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+    } catch(Exception e) {
+        e.getMessage();
+    }
+
+  }
+}
