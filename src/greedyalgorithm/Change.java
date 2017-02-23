a34df8ceae25a2bfae89cd551156a2f104075de2
papillon <devopreet.ch@gmail.com>
2017-02-23 09:06:04 -0600

    week 3

---
+++ src/greedyalgorithm/Change.java
@@ -0,0 +1,26 @@
+package greedyalgorithm;
+
+import java.util.Scanner;
+
+public class Change {
+    private static int getChange(int m) {
+        //write your code here
+
+        //no restriction such that exchanged coins must be min etc...
+        if(m>=0 & m<5){
+            return m;
+        } else if(m>=5 & m<10){
+            return 1 + getChange(m-5);
+        } else {
+            return 1 + getChange(m-10);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        System.out.println(getChange(m));
+
+    }
+}
+
