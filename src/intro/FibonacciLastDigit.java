480adfea4b2a7fad73a2c4a188e912b3f73eed77
papillon <devopreet.ch@gmail.com>
2017-03-04 13:33:25 -0600

    week 2

--- src/intro/FibonacciLastDigit.java
+++ src/intro/FibonacciLastDigit.java
@@ -4,9 +4,19 @@ import java.util.*;
 
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigitNaive(int n) {
-        if (n <= 1)
-        return n;
-
+        int[] arr = new int[n+1];
+        if (n <= 1){
+            arr[n]=n;
+            return arr[n];
+        }
+        if(n>1){
+            arr[0]=0;
+            arr[1]=1;
+            for(int i=2;i<arr.length;i++){
+                arr[i]=(arr[i-1]+arr[i-2])%10;
+            }
+        }
+/*
         int previous = 0;
         int current  = 1;
 
@@ -14,9 +24,11 @@ public class FibonacciLastDigit {
             int tmp_previous = previous;
             previous = current;
             current = ((tmp_previous%10) + (current%10))%10;
-        }
+        }*/
 
-        return current;
+
+
+        return arr[n];
         //return current;
     }
     
